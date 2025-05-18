c25d63529d29116c8eb27d38432399d692dfab5f
soomelauri <las623@lehigh.edu>
2025-05-18 00:26:09 -0400

    change

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Bms8hzqm1!@localhost/fast-api-demo'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:@localhost/fast-api-demo'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
