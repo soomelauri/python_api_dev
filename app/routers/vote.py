cbd759380370851e2a78a674f0b4637a4d8e1eda
soomelauri <las623@lehigh.edu>
2025-06-12 23:10:56 -0400

    vote functionality update

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session
 from ..database import get_db
 
 router = APIRouter(
-    prefix="/votes",
+    prefix="/vote",
     tags=['Votes']
 )
 
@@ -13,6 +13,11 @@ router = APIRouter(
 def vote(vote: schemas.Vote, 
          db: Session = Depends(get_db), 
          current_user: int = Depends(oauth2.get_current_user)):
+    
+    # check if post exists
+    post = db.query(models.Post).filter(models.Post.id == vote.post_id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {vote.post_id} does not exist")
     
     vote_query = db.query(models.Vote).filter(models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user.id)
     found_vote = vote_query.first()
@@ -24,6 +29,10 @@ def vote(vote: schemas.Vote,
         db.add(new_vote)
         db.commit()
         return {"message": "successfully added vote"}
-        
     else:
-
+        if not found_vote:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Vote does not exist")
+        
+        vote_query.delete(synchronize_session=False)
+        db.commit()
+        return {"message": "successfully removed vote"}
