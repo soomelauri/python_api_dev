68c07bc1db37c13af7a865854407c3d5da819aa3
soomelauri <las623@lehigh.edu>
2025-07-03 10:02:20 -0400

    Add CORS middleware and requirements file
    Added FastAPI CORS middleware to allow cross-origin requests from any origin. Introduced requirements.txt to specify project dependencies.

--- app/main.py
+++ app/main.py
@@ -3,11 +3,24 @@ from . import models
 from .database import engine
 from .routers import user, post, auth, vote
 from .config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 # models.Base.metadata.create_all(bind=engine)
 
+# middleware is a function that runs before every request
+
 # to create an instance, run app = FastAPI()
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
